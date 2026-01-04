04e1b8023fe75425a934d89930ec34925b753a41
colour008 <849815609@qq.com>
2026-01-04 21:51:40 +0800

    修改样式

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -10,15 +10,18 @@ export default defineConfig({
 	title: 'JamHoo的笔记',
 	description: '我的个人笔记站点',
 	lastUpdated: {
-    // 原生 Date 格式化函数
-    format: (time) => {return new Date(time).toLocaleString('zh-CN', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false})
-    },
+		// 原生 Date 格式化函数
+		format: (time) => {
+			return new Date(time).toLocaleString('zh-CN', {
+				year: 'numeric',
+				month: '2-digit',
+				day: '2-digit',
+				hour: '2-digit',
+				minute: '2-digit',
+				hour12: false,
+			});
+		},
+	},
 	// 2. 主题配置（核心：设置中文 locale）
 	themeConfig: {
 		outlineTitle: '本页目录',
